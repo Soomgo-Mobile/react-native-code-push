46bd8beb8a1f161a3c450a562cb60bad045ca3ce
Floyd Kim <for12gm@gmail.com>
2025-11-18 14:31:14 +0900

    --wip-- [skip ci]

--- cli/commands/releaseCommand/release.ts
+++ cli/commands/releaseCommand/release.ts
@@ -1,8 +1,10 @@
 import fs from "fs";
 import path from "path";
+import yauzl from "yauzl";
 import { bundleCodePush } from "../bundleCommand/bundleCodePush.js";
 import { addToReleaseHistory } from "./addToReleaseHistory.js";
 import type { CliConfigInterface } from "../../../typings/react-native-code-push.d.ts";
+import { generatePackageHashFromDirectory } from "../../utils/hash-utils.js";
 
 export async function release(
     bundleUploader: CliConfigInterface['bundleUploader'],
@@ -22,11 +24,19 @@ export async function release(
     skipBundle: boolean,
     skipCleanup: boolean,
     bundleDirectory: string,
+    hashCalc?: boolean,
 ): Promise<void> {
     const bundleFileName = skipBundle
         ? readBundleFileNameFrom(bundleDirectory)
         : await bundleCodePush(framework, platform, outputPath, entryFile, jsBundleName, bundleDirectory);
     const bundleFilePath = `${bundleDirectory}/${bundleFileName}`;
+
+    const packageHash = await (() => {
+        if (skipBundle && hashCalc) {
+            return calcHashFromBundleFile(bundleFilePath);
+        }
+        return bundleFileName;
+    })();
 
     const downloadUrl = await (async () => {
         try {
@@ -42,7 +52,7 @@ export async function release(
         appVersion,
         binaryVersion,
         downloadUrl,
-        bundleFileName,
+        packageHash,
         getReleaseHistory,
         setReleaseHistory,
         platform,
@@ -69,4 +79,43 @@ function readBundleFileNameFrom(bundleDirectory: string): string {
     }
     const bundleFilePath = path.join(bundleDirectory, files[0]);
     return path.basename(bundleFilePath);
+}
+
+function calcHashFromBundleFile(bundleFilePath: string): Promise<string> {
+    const tempDir = path.join(path.dirname(bundleFilePath), 'temp_contents_for_hash_calc');
+    console.log('🔥 🔥 🔥 tempDir', tempDir);
+    fs.mkdirSync(tempDir);
+
+    // unzip
+    yauzl.open(bundleFilePath, { lazyEntries: true }, (err, zipFile) => {
+        if (err) throw err;
+        zipFile.readEntry();
+        zipFile.on("entry", (entry) => {
+            if (/\/$/.test(entry.fileName)) {
+                // Directory file names end with '/'.
+                // Note that entries for directories themselves are optional.
+                // An entry's fileName implicitly requires its parent directories to exist.
+                zipFile.readEntry();
+            } else {
+                // file entry
+                zipFile.openReadStream(entry, (err, readStream) => {
+                    if (err) throw err;
+                    readStream.on("end", () => {
+                        zipFile.readEntry();
+                    });
+                    readStream.pipe(fs.createWriteStream(tempDir));
+                });
+            }
+        });
+    });
+
+    // calc hash
+    const hash = generatePackageHashFromDirectory(tempDir, path.join(tempDir, '..'));
+
+    // cleanup
+    fs.rmSync(tempDir, { recursive: true });
+
+    console.log('🔥 🔥 🔥 hash', hash);
+
+    return hash;
 }
