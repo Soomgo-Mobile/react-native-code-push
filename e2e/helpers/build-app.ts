869dabb51baa3c4de153a2634a03ec354b6493ed
floydkim <for12gm@gmail.com>
2026-02-17 23:35:54 +0900

    fix(e2e): use ad-hoc signing for iOS simulator instead of disabling signing

--- e2e/helpers/build-app.ts
+++ e2e/helpers/build-app.ts
@@ -20,7 +20,7 @@ async function buildIos(appPath: string, simulator?: string): Promise<void> {
     "react-native", "run-ios",
     "--mode", "Release",
     "--no-packager",
-    "--extra-params", "CODE_SIGNING_ALLOWED=NO",
+    "--extra-params", "CODE_SIGN_IDENTITY=- CODE_SIGNING_REQUIRED=NO",
   ];
   if (simulator) {
     args.push("--simulator", simulator);
